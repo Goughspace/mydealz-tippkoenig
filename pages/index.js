bf3f58bc44c04cd20cc932452c86e200bc611b60
Goughspace <pumaking8314@gmail.com>
2025-07-09 17:05:29 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-// Main app code placeholder
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to Tippkönig 🏆</h1>
+      <p>This is your new Next.js prediction app!</p>
+    </div>
+  );
+}// Main app code placeholder
