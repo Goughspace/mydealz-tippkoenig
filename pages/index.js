06253ca3d27027bedc4c804a79ff97b0738e1323
Goughspace <pumaking8314@gmail.com>
2025-07-09 17:25:14 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
-      <h1>Welcome to Tippkönig 🏆</h1>
-      <p>This is your new Next.js prediction app!</p>
-    </div>
-  );
-}
